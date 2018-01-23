9bcf32d68999a71516b798ea41cad1ba9bc0735b
Adnan Issadeen <eddyrox1@hotmail.com>
2018-01-23 12:17:02 +0530

    Added code to read statuses in from a yaml formatted document
    This adds the dependency of gopkg.in/yaml.v2. Used to read a yaml
    based text document and convert that into a Status type struct

--- setstatus.go
+++ setstatus.go
@@ -1,7 +1,24 @@
-package ssucore	
+package ssucore
+
+import (
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
 
 type Status struct {
-	statusName string
-	emoji string
-	status string
+	StatusName string `yaml:"statusName"`
+	Emoji      string `yaml:"emoji"`
+	StatusText string `yaml:"statusText"`
+}
+
+func ConvertTextToStructArray(textToConvert string) []Status {
+
+	var statusesToReturn []Status
+	err := yaml.Unmarshal([]byte(textToConvert), &statusesToReturn)
+	if err != nil {
+		log.Fatalf("couldn't convert it %s", err)
+	}
+
+	return statusesToReturn
 }
--- setstatus_test.go
+++ setstatus_test.go
@@ -1,10 +1,48 @@
 package ssucore
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestSlackStatusCanBeInitializedCorrectly(t *testing.T){
-	statusToTest := Status{statusName: "lunch", emoji: "chompy", status: "eating lunch"}
-	if statusToTest.statusName != "lunch" {
-		t.Errorf("Status name was %s. Expected lunch", statusToTest.statusName)
+func TestSlackStatusCanBeInitializedCorrectly(t *testing.T) {
+	var statusToTest Status = Status{StatusName: "lunch", Emoji: "chompy", StatusText: "eating lunch"}
+	if statusToTest.StatusName != "lunch" {
+		t.Fatalf("Status name was %s. Expected lunch", statusToTest.StatusName)
 	}
+}
+
+func TestSlackStatusCanBeReadCorrectlyFromString(t *testing.T) {
+	textToReadIn := `
+- statusName: lunch
+  emoji: chompy
+  statusText: Having lunch
+
+- statusName: resting
+  emoji: bath
+  statusText: Resting
+
+- statusName: awesome
+  emoji: awesome
+  statusText: ''
+
+- statusName: deep-work
+  emoji: ''
+  statusText: In Focus mode
+`
+	var statusesToTest []Status = ConvertTextToStructArray(textToReadIn)
+	if len(statusesToTest) != 4 {
+		t.Fatalf("Length of returned array was %d. Expected 4", len(statusesToTest))
+	} else {
+		var statusToTest Status = statusesToTest[0]
+		if statusToTest.StatusName != "lunch" {
+			t.Fatalf("Error when checking status 0: Status name was %s. Expected lunch", statusToTest.StatusName)
+		}
+		if statusToTest.Emoji != "chompy" {
+			t.Fatalf("Error when checking status 0: Emoji was %s. Expected chompy", statusToTest.Emoji)
+		}
+		if statusToTest.StatusText != "Having lunch" {
+			t.Fatalf("Error when checking status 0: Status text was %s. Expected Having lunch", statusToTest.StatusText)
+		}
+	}
+
 }
